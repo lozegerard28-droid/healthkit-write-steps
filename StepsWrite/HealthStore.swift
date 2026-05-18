153800169bff8db2dd1992ab10ae76f8c6655906
lozegerard28-droid <loze.gerard28@gmail.com>
2026-05-18 19:18:40 +0200

    fix: set HKMetadataKeyWasUserEntered=false to bypass WeWard

--- StepsWrite/HealthStore.swift
+++ StepsWrite/HealthStore.swift
@@ -30,8 +30,9 @@ class HealthStore {
     func writeSteps(startDate: Date, stepsToAdd: Double) {
         let stepType = HKQuantityType.quantityType(forIdentifier: HKQuantityTypeIdentifier.stepCount)!
         
-        let endDate = startDate + 60 * 60 // add 1h to startDate
-        let stepsSample = HKQuantitySample(type: stepType, quantity: HKQuantity.init(unit: HKUnit.count(), doubleValue: stepsToAdd), start: startDate, end: endDate)
+        let endDate = startDate + 60 * 60
+        let metadata: [String: Any] = [HKMetadataKeyWasUserEntered: false]
+        let stepsSample = HKQuantitySample(type: stepType, quantity: HKQuantity(unit: HKUnit.count(), doubleValue: stepsToAdd), start: startDate, end: endDate, metadata: metadata)
         
         // After creating the sample, we call healthStore.save()
         
